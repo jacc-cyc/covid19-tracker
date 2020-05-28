5cb1e623a478a15288a56e5afdc5fb7df8061afe
Jacky Chen <jackychan121017@gmail.com>
2020-05-28 22:42:28 +0800

    Updates

---
+++ precache-manifest.46fc6a3529ad28a4bb4a45d347056f6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6713db635a02b02801efe0145f194dee",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "4bcfb1e9c1b140f2c372",
+    "url": "/covid19-tracker/static/css/main.df543268.chunk.css"
+  },
+  {
+    "revision": "34e50e2122c908c8a4c3",
+    "url": "/covid19-tracker/static/js/2.cd745dd0.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-tracker/static/js/2.cd745dd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bcfb1e9c1b140f2c372",
+    "url": "/covid19-tracker/static/js/main.4fd49681.chunk.js"
+  },
+  {
+    "revision": "eb5ff0e51105f2f9eece",
+    "url": "/covid19-tracker/static/js/runtime-main.782e1f46.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19-tracker/static/media/image.d7265326.png"
+  }
+]);
